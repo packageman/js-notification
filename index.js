11144434ee517121bd7d317c0abb84a36080ce03
Byron Zhang <xiaoqi_2591@outlook.com>
2017-08-18 23:25:19 +0800

    refine check permission logic

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ function notify(title, content) {
     }  
     else if("Notification" in window){  
         // 判断是否有权限  
-        if (Notification.permission === "granted" || Notification.permission === "default") {  
+        if (Notification.permission === "granted") {
             var notification = new Notification(title, {  
                 "icon": iconUrl,  
                 "body": content,  
             });  
         }  
         //如果没权限，则请求权限  
-        else if (Notification.permission !== 'denied') {  
+        else if (Notification.permission !== 'denied') {
             Notification.requestPermission(function(permission) {  
                 // Whatever the user answers, we make sure we store the  
                 // information  
